39ea698d6a9b6e00a340def0865e429dc51175d7
Lanre Ogunmola <lanreogunmola@gmail.com>
2024-12-02 22:17:18 -0500

    modified python code

--- src/workload-python.py
+++ src/workload-python.py
@@ -27,31 +27,13 @@ credentialConfig = {
     "service_account_impersonation_url": "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/{SA}:generateAccessToken"
 }
 
-
-# Constants for Primus and Secundus (replace with actual values)
-# PRIMUS_BUCKET_NAME = "PRIMUS_INPUT_STORAGE_BUCKET"
-# PRIMUS_DATA_PATH = "primus_enc_customer_list.csv"
-# PRIMUS_KEY_NAME = "projects/PRIMUS_PROJECT_ID/locations/global/keyRings/PRIMUS_ENC_KEYRING/cryptoKeys/PRIMUS_ENC_KEY"
-# PRIMUS_WIP_PROVIDER_NAME = "projects/PRIMUS_PROJECT_NUMBER/locations/global/workloadIdentityPools/PRIMUS_WORKLOAD_IDENTITY_POOL/providers/PRIMUS_WIP_PROVIDER"
-# PRIMUS_KEY_ACCESS_SERVICE_ACCOUNT_EMAIL = "PRIMUS_SERVICE_ACCOUNT@PRIMUS_PROJECT_ID.iam.gserviceaccount.com"
-
-# SECUNDUS_BUCKET_NAME = "SECUNDUS_INPUT_STORAGE_BUCKET"
-# SECUNDUS_DATA_PATH = "secundus_enc_customer_list.csv"
-# SECUNDUS_KEY_NAME = "projects/SECUNDUS_PROJECT_ID/locations/global/keyRings/SECUNDUS_ENC_KEYRING/cryptoKeys/SECUNDUS_ENC_KEY"
-# SECUNDUS_WIP_PROVIDER_NAME = "projects/SECUNDUS_PROJECT_NUMBER/locations/global/workloadIdentityPools/SECUNDUS_WORKLOAD_IDENTITY_POOL/providers/SECUNDUS_WIP_PROVIDER"
-# SECUNDUS_KEY_ACCESS_SERVICE_ACCOUNT_EMAIL = "SECUNDUS_SERVICE_ACCOUNT@SECUNDUS_PROJECT_ID.iam.gserviceaccount.com"
-
-# PRIMUS_BUCKET_NAME = "primus-pj-01-input-buc"
-# PRIMUS_DATA_PATH = "primus_enc_customer_list.csv"
-# PRIMUS_KEY_NAME = "projects/primus-pj-01/locations/global/keyRings/primus-pj-01-enc-kr/cryptoKeys/primus-pj-01-enc-key-01"
-# PRIMUS_WIP_PROVIDER_NAME = "projects/675427998709/locations/global/workloadIdentityPools/primus-pj-01-wip/providers/primus-pj-01-prov"
-# PRIMUS_KEY_ACCESS_SERVICE_ACCOUNT_EMAIL = "primus-pj-01-sa@primus-pj-01.iam.gserviceaccount.com"
-
-# SECUNDUS_BUCKET_NAME = "secundus-pj-01-input-buc"
-# SECUNDUS_DATA_PATH = "secundus_enc_customer_list.csv"
-# SECUNDUS_KEY_NAME = "projects/secundus-pj-01/locations/global/keyRings/secundus-pj-01-enc-kr/cryptoKeys/secundus-pj-01-enc-key-01"
-# SECUNDUS_WIP_PROVIDER_NAME = "projects/22317815467/locations/global/workloadIdentityPools/secundus-pj-01-wip/providers/secundus-pj-01-prov"
-# SECUNDUS_KEY_ACCESS_SERVICE_ACCOUNT_EMAIL = "secundus-pj-01-sa@secundus-pj-01.iam.gserviceaccount.com"
+def write_creds(data: dict, filename: str):
+    try:
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)  # Use indent for pretty printing
+        print(f"Dictionary written to {filename}")
+    except Exception as e:
+        print(f"Error writing to JSON file: {e}")
 
 
 def replace_placeholders(data: dict, replacements: dict) -> dict:
@@ -69,12 +51,13 @@ def decrypt_data(key_name, trusted_service_account_email, wip_provider_name, enc
     
     """Decrypts the given encrypted data using the provided KMS key."""
     replacements = {
-        "wip": wip_provider_name,
-        "SA": trusted_service_account_email
+        "wip": wip_provider_name.strip("/").strip('"'),
+        "SA": trusted_service_account_email.strip("/").strip('"'),
         }
-    credential_config = json.dumps(replace_placeholders(credentialConfig, replacements))
-
-    kms_client = kms.KeyManagementServiceClient(credentials=credential_config)
+    credential_config = dict(replace_placeholders(credentialConfig, replacements))
+    filename = "config.json"
+    write_creds(credential_config, filename)
+    kms_client = kms.KeyManagementServiceClient(credentials="config.json")
 
     ciphertext_crc32c = crc32c(encrypted_data)
 
@@ -136,11 +119,11 @@ def read_from_bucket(table_info):
 def read_in_primus_table():
     """Reads and decrypts data from the Primus table."""
     primus_table_info = {
-        "bucket_name": config.get('PRIMUS','PRIMUS_BUCKET_NAME'),
-        "data_path": config.get('PRIMUS','PRIMUS_DATA_PATH'),
-        "key_name": config.get('PRIMUS','PRIMUS_KEY_NAME'),
-        "key_access_service_account_email": config.get('PRIMUS','PRIMUS_KEY_ACCESS_SERVICE_ACCOUNT_EMAIL'),
-        "wip_provider_name": config.get('PRIMUS','PRIMUS_WIP_PROVIDER_NAME'),
+        "bucket_name": config.get('PRIMUS','PRIMUS_BUCKET_NAME').strip('"'),
+        "data_path": config.get('PRIMUS','PRIMUS_DATA_PATH').strip('"'),
+        "key_name": config.get('PRIMUS','PRIMUS_KEY_NAME').strip('"'),
+        "key_access_service_account_email": config.get('PRIMUS','PRIMUS_KEY_ACCESS_SERVICE_ACCOUNT_EMAIL').strip('"'),
+        "wip_provider_name": config.get('PRIMUS','PRIMUS_WIP_PROVIDER_NAME').strip('"'),
     }
     return read_from_bucket(primus_table_info)
 
@@ -148,11 +131,11 @@ def read_in_primus_table():
 def read_in_secundus_table():
     """Reads and decrypts data from the Secundus table."""
     secundus_table_info = {
-        "bucket_name": config.get('SECUNDUS','SECUNDUS_BUCKET_NAME'),
-        "data_path": config.get('SECUNDUS','SECUNDUS_DATA_PATH'),
-        "key_name": config.get('SECUNDUS','SECUNDUS_KEY_NAME'),
-        "key_access_service_account_email": config.get('SECUNDUS','SECUNDUS_KEY_ACCESS_SERVICE_ACCOUNT_EMAIL'),
-        "wip_provider_name": config.get('SECUNDUS','SECUNDUS_WIP_PROVIDER_NAME'),
+        "bucket_name": config.get('SECUNDUS','SECUNDUS_BUCKET_NAME').strip('"'),
+        "data_path": config.get('SECUNDUS','SECUNDUS_DATA_PATH').strip('"'),
+        "key_name": config.get('SECUNDUS','SECUNDUS_KEY_NAME').strip('"'),
+        "key_access_service_account_email": config.get('SECUNDUS','SECUNDUS_KEY_ACCESS_SERVICE_ACCOUNT_EMAIL').strip('"'),
+        "wip_provider_name": config.get('SECUNDUS','SECUNDUS_WIP_PROVIDER_NAME').strip('"'),
     }
     return read_from_bucket(secundus_table_info)
 
@@ -183,10 +166,11 @@ def main():
         [total]
     ]
     blob_name = "result.csv"
-    result_bucket = config.get('SECUNDUS','SECUNDUS_RESULT_STORAGE_BUCKET')
+    result_bucket = config.get('SECUNDUS','SECUNDUS_RESULT_STORAGE_BUCKET').strip('"')
     storage_client = storage.Client()
     bucket = storage_client.bucket(result_bucket)
     blob = bucket.blob(blob_name)
+    
     # Write data to a string buffer in CSV format
     csv_buffer = StringIO()
     writer = csv.writer(csv_buffer)
